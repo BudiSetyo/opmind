79d857d4c593e362c52c1490d46b0cf8aaa68245
BudiSetyo <budisetyo176@gmail.com>
2021-05-07 14:05:14 +0700

    add components and screens

--- src/screens/login/Login.js
+++ src/screens/login/Login.js
@@ -1,23 +1,26 @@
 import React from 'react';
-import {
-  Container,
-  Header,
-  Body,
-  Title,
-  Footer,
-  Text,
-  StyleProvider,
-} from 'native-base';
-import {StyleSheet} from 'react-native';
+import {View, Text, TextInput, StyleSheet} from 'react-native';
+import Input from '../../components/input/Input';
 
 const Login = () => {
   return (
-    <Container>
-      <Header>
-        <Title>Login</Title>
-      </Header>
-    </Container>
+    <View style={styles.container}>
+      <View>
+        <Text>Login</Text>
+      </View>
+      <View>
+        <TextInput secureTextEntry={true} placeholder="Username" />
+      </View>
+      <Input placeholder="input" type="password" />
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#E5E5E5',
+  },
+});
 
 export default Login;
