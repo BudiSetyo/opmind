013b79142658f9f3709f147bbd28f492548c38ed
BudiSetyo <budisetyo176@gmail.com>
2021-05-07 08:16:38 +0700

    initial commit

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+// import {Container, Content, Text} from 'native-base';
+import Login from './src/screens/login/Login';
+
+const App = () => {
+  return <Login />;
+};
+
+export default App;
---
+++ src/screens/login/Login.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+  Container,
+  Header,
+  Body,
+  Title,
+  Footer,
+  Text,
+  StyleProvider,
+} from 'native-base';
+import {StyleSheet} from 'react-native';
+
+const Login = () => {
+  return (
+    <Container>
+      <Header>
+        <Title>Login</Title>
+      </Header>
+    </Container>
+  );
+};
+
+export default Login;
